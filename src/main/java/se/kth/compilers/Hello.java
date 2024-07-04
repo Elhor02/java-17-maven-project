2c5846d64612cfdef3d91e9d185d9058a44b04d2
Ayman El Hor <110869519+Elhor02@users.noreply.github.com>
2024-07-04 12:31:13 +0100

    Update Hello.java

--- src/main/java/se/kth/compilers/Hello.java
+++ src/main/java/se/kth/compilers/Hello.java
@@ -1,6 +1,6 @@
 package se.kth.compilers;
 
-public classe Hello {
+public class Hello {
   public static void main(String[] args) {
     if (args.length > 0) {
       System.out.printf("Hello %s!", args[0]);
